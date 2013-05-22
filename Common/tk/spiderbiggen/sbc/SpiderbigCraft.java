8915840f09b8a455a55a3c10508269b7505dd1a9
spiderbiggen <spiderbiggen@gmail.com>
2013-05-22 23:00:35 +0200

    main mod class and some icons

---
+++ Common/tk/spiderbiggen/sbc/SpiderbigCraft.java
@@ -0,0 +1,35 @@
+package tk.spiderbiggen.sbc;
+
+import tk.spiderbiggen.sbc.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(   modid = Reference.MOD_ID,
+        name = Reference.MOD_NAME,
+        version = Reference.VERSION)
+
+public class SpiderbigCraft {
+    
+    @PreInit
+    public void preInit(FMLPreInitializationEvent event){
+        
+    }
+    
+    @Init
+    public void preInit(FMLInitializationEvent event){
+        
+    }
+    
+    @PostInit
+    public void preInit(FMLPostInitializationEvent event){
+        
+    }
+    
+    
+    
+}
---
+++ Common/tk/spiderbiggen/sbc/lib/Reference.java
@@ -0,0 +1,9 @@
+package tk.spiderbiggen.sbc.lib;
+
+public class Reference {
+    public static final String MOD_ID = "SBC";
+    public static final String MOD_NAME = "SpiderbigCraft";
+    public static final String VERSION = "PRE 0.0.1";
+    
+
+}
